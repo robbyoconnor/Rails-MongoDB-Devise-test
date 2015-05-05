8385e58a2f7de55d152a84c187e6024e046bd82f
Rodrigo Argumedo <rodrigo.argumedo@outlook.com>
2015-05-05 23:33:11 +0000

    Create and destroy posts, permit certain..
    items to allow the creation and modification of blog posts.

--- app/controllers/blog/posts_controller.rb
+++ app/controllers/blog/posts_controller.rb
@@ -6,4 +6,26 @@ class Blog::PostsController < ApplicationController
   def new
     @post = Post.new
   end
+  
+  def create
+    @post = Post.new(post_params)
+    
+    if @post.save
+      redirect_to @post
+    else
+      render 'new'
+    end
+  end
+  
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+    
+    redirect_to blog_posts_path
+  end
+  
+  private
+  def post_params
+    params.require(:post).permit(:title, :body)
+  end
 end
