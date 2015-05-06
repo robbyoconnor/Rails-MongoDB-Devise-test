78e66fe36291124dd9f7b8d3d1903283ee41b76c
Rodrigo Argumedo <rodrigo.argumedo@outlook.com>
2015-05-06 14:39:39 +0000

    Removed blog post dirs

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,5 @@
 Rails.application.routes.draw do
   root 'home#index'
-  namespace :blog do
-    resources :posts
-  end
 
   devise_for :users
   # The priority is based upon order of creation: first created -> highest priority.
