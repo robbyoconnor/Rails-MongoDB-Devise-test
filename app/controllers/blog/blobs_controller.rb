7647d64570ec0ae6e7c531c91412c1120e04fa57
Rodrigo Argumedo <rodrigo.argumedo@icloud.com>
2015-05-07 19:48:07 +0000

    Add basic elements for blogging

--- app/controllers/blog/blobs_controller.rb
+++ app/controllers/blog/blobs_controller.rb
@@ -1,2 +1,28 @@
 class Blog::BlobsController < ApplicationController
+    def index
+       @blobs = Blog::Blob.all
+    end
+    
+    def new
+      @blob = Blog::Blob.new
+    end
+    
+    def create
+      @blob = Blog::Blob.new(blob_params)
+      
+      if @blob.save
+          redirect_to @blob
+      else
+          render 'new'
+      end
+    end
+    
+    def show
+      @blob = Blog::Blob.find(params[:id])
+    end
+    
+    private
+    def blob_params
+      params.require(:blob).permit(:title, :body)
+    end
 end
