4bbdfea8c964d274a9a8a1f3e837689b141f5684
Rodrigo Argumedo <rodrigo.argumedo@icloud.com>
2015-05-07 17:55:32 +0000

    Renamed Blob model

--- app/models/blog/blob.rb
+++ app/models/blog/blob.rb
@@ -1,4 +1,4 @@
-class Blob
+class Blog::Blob
   include Mongoid::Document
   field :title, type: String
   field :body, type: String
--- test/models/blog/blob_test.rb
+++ test/models/blog/blob_test.rb
@@ -1,6 +1,6 @@
 require 'test_helper'
 
-class BlobTest < ActiveSupport::TestCase
+class Blog::BlobTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
