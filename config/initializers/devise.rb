450970625149bfe9744b6239f143fd62356c6a6d
Rodrigo Argumedo <rodrigo.argumedo@icloud.com>
2015-05-18 10:34:38 -0600

    [skip ci] Note on Facebook API

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -233,6 +233,7 @@ Devise.setup do |config|
   # Add a new OmniAuth provider. Check the wiki for more information on setting
   # up on your models and hooks.
   # config.omniauth :github, 'APP_ID', 'APP_SECRET', scope: 'user,public_repo'
+  # You need this to properly work with your test app
   # config.omniauth :facebook, 'APP_ID', 'APP_SECRET'
 
   # ==> Warden configuration
