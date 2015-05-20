e274edea5134151a2e266e415b66c1f708919780
Rodrigo Argumedo <rodrigo.argumedo@icloud.com>
2015-05-20 09:48:17 -0600

    Add blank routes

--- app/controllers/project_controller.rb
+++ app/controllers/project_controller.rb
@@ -1,2 +1,19 @@
 class ProjectController < ApplicationController
+    def index
+    end
+    
+    def new
+    end
+    
+    def show
+    end
+    
+    def edit
+    end
+    
+    def update
+    end
+    
+    def destroy
+    end
 end
